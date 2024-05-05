b174ac6861b1cd6621e588f82dbb2a0b843c5a01
SandraS2611 <sssandra1980@gmail.com>
2024-05-05 18:37:21 -0300

    arreglo des9

---
+++ prueba.py
@@ -0,0 +1,20 @@
+# import sys
+
+# num1 = 10000/1000
+# num2 = -2
+# print(sys.getsizeof(num1), type(num1))
+
+# print(sys.getsizeof(num2), type(num2))
+
+num1 = 4
+num2 = 12
+resultado = num1 & num2
+
+print(f"el resultado es: {resultado}")
+
+
+num1 = 12
+num2 = 4
+resultado = num1 + num2
+
+print(f"el resultado es: {resultado}")
