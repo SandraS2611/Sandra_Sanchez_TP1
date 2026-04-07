cb99440a2db05a121a88cc94837651a222ec2e51
SandraS2611 <sssandra1980@gmail.com>
2026-04-07 15:17:35 -0300

    desafio 1

--- desafío1.py
+++ desafío1.py
@@ -1,7 +1,23 @@
 """Desafío 1
-Solicitar al usuario que ingrese su número de cliente. Si el número es el 1000, imprimir "Ganaste un premio"."""
+Solicitar al usuario que ingrese su número de cliente. Si el número es el 100, imprimir
+"Ganaste un premio".
 
-nroCliente = int(input("Ingrese el número de cliente: "))
+numero_ingresado = int(input("Ingrese el número de cliente: "))
 
-if nroCliente == 1000:
+if numero_ingresado == 100:
   print("Ganaste un premio")
+else:
+  print("Probá de nuevo.")
+"""
+  
+# WHILE
+numero_ingresado = 0
+
+while numero_ingresado != 100:
+  numero_ingresado = int(input("Ingrese el número de cliente: "))
+
+  if numero_ingresado == 100:
+    print("Ganaste un premio.")
+  else:
+    print("Probá de nuevo. \n")
+    
